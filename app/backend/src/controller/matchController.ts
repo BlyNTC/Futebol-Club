a1e94328dd5c97c572de98f983392fd9f73a4104
Isaac Matheus <isaacase@hotmail.com>
2022-03-16 22:16:11 -0300

    implementa metodo get  da rota Match

--- app/backend/src/controller/matchController.ts
+++ app/backend/src/controller/matchController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import { ResponseAndStatus } from '../interfaces';
 import * as matchService from '../services/matchService';
+// import { validateToken } from '../utils';
 
 export async function getAll(req: Request, res: Response): Promise<void> {
-  const userFinded: ResponseAndStatus = await matchService.getAll();
+  const userFinded: ResponseAndStatus = await matchService.getAll(req.query);
+  console.log('REQ.QUERY ========================>>', req.query);
   res.status(userFinded.status).json(userFinded.response);
 }
 
@@ -11,3 +13,8 @@ export async function getById(req: Request, res: Response): Promise<void> {
   const userFinded: ResponseAndStatus = await matchService.getbyId(parseInt(req.params.id, 10));
   res.status(userFinded.status).json(userFinded.response);
 }
+
+// export async function createMatch(req: Request, res: Response): Promise<void> {
+//   const MatchCreated = matchService.createMatch(req);
+//   res.status(MatchCreated.status).json(MatchCreated.response);
+// }
--- app/backend/src/services/matchService.ts
+++ app/backend/src/services/matchService.ts
@@ -1,9 +1,23 @@
-import Match from '../database/models/Match';
 import Club from '../database/models/Club';
+import Match from '../database/models/Match';
+// import { ResponseAndStatus } from '../interfaces';
 
-export async function getAll() {
-  const allMatchs: any[] = await Match.findAll({ include: Club });
-  console.log('ALLMATCHS =====================>>>', allMatchs);
+export async function getAll(query: any) {
+  if (query) {
+    const inProgress = query.inProgress === 'true';
+    const allMatchs: Match[] = await Match.findAll({
+      where: { in_progress: inProgress },
+      include: [
+        { model: Club, as: 'awayClub', attributes: [['club_name', 'clubName']] },
+        { model: Club, as: 'homeClub', attributes: [['club_name', 'clubName']] },
+      ] });
+    return { response: allMatchs, status: 200 };
+  }
+  const allMatchs: Match[] = await Match.findAll({
+    include: [
+      { model: Club, as: 'awayClub', attributes: [['club_name', 'clubName']] },
+      { model: Club, as: 'homeClub', attributes: [['club_name', 'clubName']] },
+    ] });
   return { response: allMatchs, status: 200 };
 }
 
@@ -21,3 +35,7 @@ export async function getbyId(id: number | string) {
   }
   return { response: MatchsFinded, status: 200 };
 }
+
+// export async function createMatch(reqBody): ResponseAndStatus {
+//   return { response:  , status: 200 }
+// }
